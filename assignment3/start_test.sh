281e85bfe29db75355fcb63c06326daaeb372cea
cmalik3595 <cmalik3595@sdsu.edu>
2022-10-05 01:07:26 -0700

    Download connector jar and put in venv

--- assignment3/start_test.sh
+++ assignment3/start_test.sh
@@ -10,9 +10,14 @@ function cleanup {
 	killall -9 mysql 2>/dev/null
 }
 
-if [[ -f ../../.venv/lib/python3.10/site-packages/pyspark/jars/mysql-connector-java-8.0.21.jar  ]]; then
+if [[ ! -f ../.venv/lib/python3.10/site-packages/pyspark/jars/mysql-connector-java-8.0.29.jar  ]]; then
        # Copy the jar to venv path. Otherwise the python-spark integration gives errors.
-	wget --quiet -c https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.21.tar.gz -O - | tar -zxvf - mysql-connector-java-8.0.21/mysql-connector-java-8.0.21.jar --strip-components=1
+       	mkdir -p tmp 
+	cd tmp
+	wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.29.tar.gz 
+	tar -xvf mysql-connector-java-8.0.29.tar.gz
+	cp mysql-connector-java-8.0.29/mysql-connector-java-8.0.29.jar ../../.venv/lib/python3.10/site-packages/pyspark/jars/mysql-connector-java-8.0.29.jar
+	cd -; rm -rf tmp
 fi
 
 source ../.venv/bin/activate
